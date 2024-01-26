48a7a68defd547fd7d553f22dd9722df099e3399
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-26 01:02:14 -0300

    feat: using default.nix in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,16 @@
-{
- description = "A flake for unimatrix";
+  {
+   description = "A flake for unimatrix";
 
- inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
- outputs = { self, nixpkgs }: {
+   outputs = { self, nixpkgs }: {
 
-  packages.x86_64-linux.unimatrix =
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    pkgs.stdenv.mkDerivation rec {
-      pname = "unimatrix";
-      version = "1.0.0";
-
-      src = self;
-
-      phases = "installPhase";
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp ${./unimatrix.py} $out/bin/unimatrix
-        chmod +x $out/bin/unimatrix
-      '';
-
-      meta = with pkgs.lib; {
-        description = "Python script to simulate the display from \"The Matrix\" in terminal. Uses half-width katakana unicode characters by default, but can use custom character sets. Accepts keyboard controls while running";
-        homepage = https://github.com/will8211/unimatrix;
-        license = licenses.gpl3;
-        maintainers = [ maintainers.will8211 ];
-        platforms = platforms.all;
-      };
-    };
+    packages.x86_64-linux.unimatrix =
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+      in
+      import ./default.nix { inherit pkgs; };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.unimatrix;
  };
