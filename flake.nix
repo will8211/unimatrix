8454a8e8e5753f5604a4ea8f194a88061c8617d6
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-02-01 22:15:07 -0300

    fix: support all systems in flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
-  {
-   description = "A flake for unimatrix";
+{
+  description = "A flake for unimatrix";
 
-   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.unimatrix =
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
+    systemPackageBuilder = system:
       let
-        system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
       in
-      import ./default.nix { inherit pkgs; };
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.unimatrix;
- };
+      {
+        ${system} = {
+          unimatrix = import ./default.nix { inherit pkgs; };
+          default = self.packages.${system}.unimatrix;
+        };
+      };
+    systemPackages = builtins.map systemPackageBuilder systems;
+  in
+  {
+    packages = builtins.foldl' (a: b: a // b) {} systemPackages;
+  };
 }
